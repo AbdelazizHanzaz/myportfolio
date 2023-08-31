dfe3d238816fae99d16338b3cf3af083fb8202c6
abdelazizhanzaz <abdelazizhanzaz>
2023-08-31 02:05:43 +0100

    some update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // Toggle mobile navbar 
 
-const toggleButton = document.querySelector('.toggle-button');
 const navLinks = document.querySelector('.nav-links');
+const navLinksItem = document.querySelectorAll('.nav-links-item');
+const menuButton = document.querySelector('.menu-button');
 
-toggleButton.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+menuButton.addEventListener('click', () => {
+      navLinks.classList.toggle("show-menu");
+});
+
+navLinksItem.forEach(item =>{
+  item.addEventListener('click', () => {
+    navLinks.classList.remove('show-menu');
+   
+  });
 });
